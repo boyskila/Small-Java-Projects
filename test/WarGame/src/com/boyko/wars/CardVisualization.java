22bd5bf9a0de96d1babd621032ee38d8ef9d9a4d
boyskila <bmlalov@abv.bg>
2015-04-30 12:30:04 +0300

    b

--- test/WarGame/src/com/boyko/wars/CardVisualization.java
+++
@@ -1,9 +0,0 @@
-package com.boyko.wars;
-
-import java.io.IOException;
-
-public class CardVisualization {
-    public CardVisualization(Card card) throws IOException {
-        new FileManager(card);
-    }
-}
